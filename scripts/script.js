66d2928bf66b5ba607deec77ccd2088656690933
misikdmitriy <misik.dmitriy@gmail.com>
2016-08-15 23:38:35 +0300

    initial

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+showDiv(slideIndex);
+
+function plusDivs(n) {
+    badgeClick(slideIndex += n);
+}
+
+function currentDiv(n) {
+    badgeClick(slideIndex = n);
+}
+
+function showDiv(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        $(x[i]).hide();
+    }
+
+    $(x[slideIndex - 1]).show("fade", {}, 500, function () {
+        for (i = 0; i < x.length; i++) {
+            if (i != slideIndex - 1) {
+                $(x[i]).hide();
+            }
+        }
+    });
+}
+
+function makeUnactive() {
+    $('.badge-active').attr('class', 'badge');
+}
+
+function badgeClick(num) {
+    showDiv(slideIndex = num);
+    makeUnactive();
+
+    var badge = $('.badge')[slideIndex - 1];
+    $(badge).attr('class', 'badge-active');
+}
